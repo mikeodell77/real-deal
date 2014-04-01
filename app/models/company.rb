aca8038faa7a08130facb985f959c2328e13b200
Mike O'Dell <mike.o'dell@pgi.com>
2014-04-01 19:20:51 -0400

    Adding companies model

---
+++ app/models/company.rb
@@ -0,0 +1,10 @@
+class Company
+  include Mongoid::Document
+  field :name, type: String
+  field :address1, type: String
+  field :address2, type: String
+  field :city, type: String
+  field :state, type: String
+  field :zip, type: String
+  field :phone, type: String
+end
