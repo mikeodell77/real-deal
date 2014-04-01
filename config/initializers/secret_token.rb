b6d0e261a84d138a2976d1b7dde870d574f4f035
Mike O'Dell <mike.o'dell@pgi.com>
2014-04-01 17:27:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealDeal::Application.config.secret_key_base = 'b70f6509811ff6bb48b7d90cacaca35f4a24851261f48980669185c6d83d45f4a1b56897481b00181cbcd8c51e1cb1642677e14f983eef40bdcaf48fcb383a11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RealDeal::Application.config.session_store :cookie_store, key: '_real-deal_session'
