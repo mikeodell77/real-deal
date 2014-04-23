da8c6f0e643622dd4a9913e0be9c47d46db3030c
Mike O'Dell <mike.o'dell@pgi.com>
2014-04-23 17:03:49 -0400

    Update twitter hash tag

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,12 @@ class WelcomeController < ApplicationController
   before_action :route_by_user_type
 
   def index
+    @companies = Company.all.to_a
   end
+
   def tweets
-    @tags = Tweets.client.search("#real-deal -rt")
+    # @tags = Tweets.client.search("#real-deal -rt")
+    @tags = Tweets.client.search("#realDealTriangle -rt")
     render layout: false
   end
 end
