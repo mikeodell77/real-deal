feb1a7722903db31b6954b07949c436a05031feb
Mike O'Dell <mike.o'dell@pgi.com>
2014-04-24 18:52:22 -0400

    default amount and percent.

--- app/models/funding.rb
+++ app/models/funding.rb
@@ -1,7 +1,7 @@
 class Funding
   include Mongoid::Document
-  field :amount, type: BigDecimal
-  field :percentage, type: Float
+  field :amount, type: BigDecimal, default: 0
+  field :percentage, type: Float, default: 0
   field :approved, type: Mongoid::Boolean
   field :email, type: String
   field :unverified, type: Boolean, default: false
